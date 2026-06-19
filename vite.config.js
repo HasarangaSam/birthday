e7e890143e0af21ef87eade22da4ba155c2ec21e
Hasaranga <hasarangasamarakoon@gmail.com>
2026-06-19 09:29:01 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  // base: process.env.VITE_BASE_PATH || "/",
+  base: process.env.VITE_BASE_PATH,
 });
